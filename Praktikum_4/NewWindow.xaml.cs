60d42d75e3e739d33d2f622eb3c1f6d9e494fbbf
TheAlexGo <alex280702@mail.ru>
2021-09-29 11:31:03 +0300

    Выполнение задания 1

---
+++ Praktikum_4/NewWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Praktikum_4
+{
+    /// <summary>
+    /// Логика взаимодействия для NewWindow.xaml
+    /// </summary>
+    public partial class NewWindow : Window
+    {
+        public NewWindow()
+        {
+            InitializeComponent();
+        }
+        public NewWindow(string title) //: this()
+        {
+            InitializeComponent(); // Переопределяя конструктор мы должны вызывать метод InitializeComponent
+            Title = title;
+        }
+
+    }
+}
