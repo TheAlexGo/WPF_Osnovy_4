91e07cd9ccb7de03d60d0fabb945acc2ccb2263a
TheAlexGo <alex280702@mail.ru>
2021-09-29 11:33:58 +0300

    Инициализация проекта

--- Praktikum_4/MainWindow.xaml.cs
+++ Praktikum_4/MainWindow.xaml.cs
@@ -24,18 +24,5 @@ namespace Praktikum_4
         {
             InitializeComponent();
         }
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            NewWindow window = new NewWindow("Модальное");
-            window.ShowDialog();
-            // Код после метода ShowDialog выполнится только тогда, кода диалоговое окно закроется.
-        }
-        private void button2_Click(object sender, RoutedEventArgs e)
-        {
-            NewWindow window = new NewWindow("Немодальное");
-            window.Show();
-            // Код после метода Show выполнится сразу.
-        }
-
     }
 }
