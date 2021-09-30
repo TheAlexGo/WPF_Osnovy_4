fbce496760c64e93a67a0ac5d5d6f16c68aaa956
TheAlexGo <alex280702@mail.ru>
2021-09-30 14:10:51 +0300

    Инициализация проекта

--- Praktikum_4/MainWindow.xaml.cs
+++ Praktikum_4/MainWindow.xaml.cs
@@ -24,15 +24,5 @@ namespace Praktikum_4
         {
             InitializeComponent();
         }
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            // Окна, имеющие окно-владельца, удобно применять для всплывающих окон и панелей инструментов.
-            // Дочернее окно всегда отображается поверх своего владельца и сворачивается, когда сворачивается окно-владелец.
-            Window1 window = new Window1();
-            window.Owner = this;
-            window.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-            window.Show();
-        }
-
     }
 }
